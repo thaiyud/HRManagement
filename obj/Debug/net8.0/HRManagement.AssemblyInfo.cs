f3f1fa608c62847cdc5d7a9735f5b8fe1918ed0a
thaiyud <thaiquoc48203@gmail.com>
2024-05-27 15:16:45 +0700

    last commit

--- obj/Debug/net8.0/HRManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HRManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6540417a4f8bfeb64ed8acaf0ff17733bd22ca9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
